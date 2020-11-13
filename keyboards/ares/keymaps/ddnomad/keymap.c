efff3bc49cf600024e48939af6229e91df4e973f
ddnomad <dd@ddworks.io>
2020-11-13 19:57:23 +0200

    Added an ability to  swap gui and alt

--- keyboards/ares/keymaps/ddnomad/keymap.c
+++ keyboards/ares/keymaps/ddnomad/keymap.c
@@ -31,9 +31,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LSFT, KC_NUBS, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_FN),
   KC_LCTL, KC_LALT, KC_LGUI,                       KC_SPC,                          KC_RGUI, KC_RALT, KC_APP,  KC_RCTRL),
 [_FN] = LAYOUT(
-  ______,  KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_INS,  RESET,
+  RESET,  KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_INS,  KC_PSCR,
   KC_CAPS, ______,  ______,  ______,  ______,  ______,  ______,  ______,  KC_PSCR, KC_SLCK, KC_PAUS,  KC_UP,   ______,  KC_DEL,
   ______,  KC_VOLD, KC_VOLU, KC_MUTE, ______,  ______,  KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT,  KC_RGHT, ______,  ______,
-  ______,  KC_MPRV, KC_MPLY, KC_MNXT, ______,  ______,  KC_PPLS, KC_PMNS, KC_END,  KC_PGDN, ______,   KC_DOWN,  ______,  ______,
-  ______,  ______,  ______,                         ______,                          ______,  ______, ______,  ______)
+  ______,  KC_MPRV, KC_MPLY, KC_MNXT, ______,  ______,  KC_PPLS, KC_PMNS, KC_END,  KC_PGDN, ______,   KC_DOWN,  ______, ______,
+  ______,  ______,  ______,                         ______,                          ______,  ______, AG_TOGG,  ______)
 };
